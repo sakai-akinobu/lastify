3e8602e1e041b24d4d50566427dea283560b6c5d
sakai-akinobu <sakai.akinobu@gmail.com>
2019-01-27 12:13:24 +0900

    Fix code style.

--- src/api/user.ts
+++ src/api/user.ts
@@ -11,7 +11,12 @@ import {
 
 const DEFAULT_LIMIT = 3;
 
-function getRequest<T>(apiKey: string, user: string, apiMethod: string, option: {period?: Period, limit?: number}): Promise<T> {
+function getRequest<T>(
+  apiKey: string,
+  user: string,
+  apiMethod: string,
+  option: {period?: Period, limit?: number} = {},
+): Promise<T> {
   const formattedUrl = url.format({
     protocol: 'http',
     hostname: 'ws.audioscrobbler.com',
@@ -45,18 +50,37 @@ function getRequest<T>(apiKey: string, user: string, apiMethod: string, option: 
   });
 }
 
-export function getRecentTracks(apiKey: string, user: string, limit: number = DEFAULT_LIMIT): Promise<RecentTracks> {
+export function getRecentTracks(
+  apiKey: string,
+  user: string,
+  limit: number = DEFAULT_LIMIT,
+): Promise<RecentTracks> {
   return getRequest<RecentTracks>(apiKey, user, 'getrecenttracks', {limit});
 }
 
-export function getTopAlbums(apiKey: string, user: string, period: Period = '1month', limit: number = DEFAULT_LIMIT): Promise<TopAlbums> {
+export function getTopAlbums(
+  apiKey: string,
+  user: string,
+  period: Period = '1month',
+  limit: number = DEFAULT_LIMIT,
+): Promise<TopAlbums> {
   return getRequest<TopAlbums>(apiKey, user, 'gettopalbums', {period, limit});
 }
 
-export function getTopArtists(apiKey: string, user: string, period: Period = '1month', limit: number = DEFAULT_LIMIT): Promise<TopArtists> {
+export function getTopArtists(
+  apiKey: string,
+  user: string,
+  period: Period = '1month',
+  limit: number = DEFAULT_LIMIT,
+): Promise<TopArtists> {
   return getRequest<TopArtists>(apiKey, user, 'gettopartists', {period, limit});
 }
 
-export function getTopTracks(apiKey: string, user: string, period: Period = '1month', limit: number = DEFAULT_LIMIT): Promise<TopTracks> {
+export function getTopTracks(
+  apiKey: string,
+  user: string,
+  period: Period = '1month',
+  limit: number = DEFAULT_LIMIT,
+): Promise<TopTracks> {
   return getRequest<TopTracks>(apiKey, user, 'gettoptracks', {period, limit});
 }
