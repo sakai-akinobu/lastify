ba2f1773ab205ccf2c075cf9a1cecd013439e7ee
sakai-akinobu <sakai.akinobu@gmail.com>
2019-01-27 11:36:00 +0900

    Add period option.

--- src/api/types.ts
+++ src/api/types.ts
@@ -55,3 +55,11 @@ export interface TopTracks {
     ];
   };
 }
+
+export type Period =
+  | 'overall'
+  | '7day'
+  | '1month'
+  | '3month'
+  | '6month'
+  | '12month';
--- src/api/user.ts
+++ src/api/user.ts
@@ -5,12 +5,13 @@ import {
   TopAlbums,
   TopArtists,
   TopTracks,
+  Period,
 } from './types';
 
 const ROOT_URL = 'http://ws.audioscrobbler.com/2.0';
 
-function getRequest<T>(apiKey: string, user: string, apiMethod: string, limit: number): Promise<T> {
-  const url = `${ROOT_URL}/?method=user.${apiMethod}&user=${user}&api_key=${apiKey}&limit=${limit}&format=json`;
+function getRequest<T>(apiKey: string, user: string, apiMethod: string, option: {period?: Period, limit?: number}): Promise<T> {
+  const url = `${ROOT_URL}/?method=user.${apiMethod}&user=${user}&api_key=${apiKey}&period=${option.period}&limit=${option.limit}&format=json`;
   return new Promise((resolve, reject) => {
     let body = '';
     http.get(url, res => {
@@ -32,17 +33,17 @@ function getRequest<T>(apiKey: string, user: string, apiMethod: string, limit: n
 }
 
 export function getRecentTracks(apiKey: string, user: string, limit: number = 5): Promise<RecentTracks> {
-  return getRequest<RecentTracks>(apiKey, user, 'getrecenttracks', limit);
+  return getRequest<RecentTracks>(apiKey, user, 'getrecenttracks', {limit});
 }
 
-export function getTopAlbums(apiKey: string, user: string, limit: number = 5): Promise<TopAlbums> {
-  return getRequest<TopAlbums>(apiKey, user, 'gettopalbums', limit);
+export function getTopAlbums(apiKey: string, user: string, period: Period = '1month', limit: number = 5): Promise<TopAlbums> {
+  return getRequest<TopAlbums>(apiKey, user, 'gettopalbums', {period, limit});
 }
 
-export function getTopArtists(apiKey: string, user: string, limit: number = 5): Promise<TopArtists> {
-  return getRequest<TopArtists>(apiKey, user, 'gettopartists', limit);
+export function getTopArtists(apiKey: string, user: string, period: Period = '1month', limit: number = 5): Promise<TopArtists> {
+  return getRequest<TopArtists>(apiKey, user, 'gettopartists', {period, limit});
 }
 
-export function getTopTracks(apiKey: string, user: string, limit: number = 5): Promise<TopTracks> {
-  return getRequest<TopTracks>(apiKey, user, 'gettoptracks', limit);
+export function getTopTracks(apiKey: string, user: string, period: Period = '1month', limit: number = 5): Promise<TopTracks> {
+  return getRequest<TopTracks>(apiKey, user, 'gettoptracks', {period, limit});
 }
