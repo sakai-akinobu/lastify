3461ebe361956fad574eec3dd9cd4229d0c321bf
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-17 11:33:57 +0900

    Add script for cli.

---
+++ bin/cli.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+const path = require('path');
+
+const main = require(path.join(__dirname, '../lib/src/index')).main;
+const parse = require(path.join(__dirname, '../lib/src/options')).parse;
+const exitWithErrorMessage = require(path.join(__dirname, '../lib/src/error-handler')).exitWithErrorMessage;
+const tableFormatter = require(path.join(__dirname, '../lib/src/table-formatter'));
+
+const {
+  apiKey,
+  user,
+  period,
+  limit,
+} = parse(process.env, process.argv);
+
+if (apiKey === undefined) {
+  exitWithErrorMessage(process, `--api-key argument is required.`);
+}
+if (user === undefined) {
+  exitWithErrorMessage(process, `--user argument is required.`);
+}
+if (period !== undefined) {
+  if (![
+    'overall',
+    '7day',
+    '1month',
+    '3month',
+    '6month',
+    '12month',
+  ].includes(period)) {
+    exitWithErrorMessage(process, `--period must either overall | 7day | 1month | 3month | 6month | 12month.`);
+  }
+}
+if (limit !== undefined) {
+  if (isNaN(Number(limit)) || Number(limit) !== parseInt(limit, 10)) {
+    exitWithErrorMessage(process, `--limit must be integer.`);
+  }
+}
+
+main(apiKey, user, period, limit)
+.then(([
+  recentTracks,
+  topAlbums,
+  topArtists,
+  topTracks,
+]) => {
+  process.stdout.write(
+    [
+      tableFormatter.recentTracks(recentTracks),
+      tableFormatter.topAlbums(topAlbums),
+      tableFormatter.topArtists(topArtists),
+      tableFormatter.topTracks(topTracks),
+    ].join('\n'),
+  );
+  process.exit(0);
+})
+.catch(e => {
+  exitWithErrorMessage(process, `${e.message}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,11 @@
-import * as tableFormatter from './table-formatter';
 import * as apiUser from './api/user';
 import {Period} from './api/types';
-import {parse} from './options';
-import {exitWithErrorMessage} from './error-handler';
 
-const {
-  apiKey,
-  user,
-  period,
-  limit,
-} = parse(process.env, process.argv);
-
-if (apiKey === undefined) {
-  exitWithErrorMessage(process, `--api-key argument is required.`);
+export async function main(apiKey: string, user: string, period: string, limit: number): Promise<any> {
+  return Promise.all([
+    apiUser.getRecentTracks(apiKey, user, limit),
+    apiUser.getTopAlbums(apiKey, user, period as Period, limit),
+    apiUser.getTopArtists(apiKey, user, period as Period, limit),
+    apiUser.getTopTracks(apiKey, user, period as Period, limit),
+  ]);
 }
-if (user === undefined) {
-  exitWithErrorMessage(process, `--user argument is required.`);
-}
-if (period !== undefined) {
-  if (![
-    Period.Overall,
-    Period.Week,
-    Period.Month,
-    Period.Quarter,
-    Period.HalfYear,
-    Period.Year,
-  ].includes(period as Period)) {
-    exitWithErrorMessage(process, `--period must either overall | 7day | 1month | 3month | 6month | 12month.`);
-  }
-}
-if (limit !== undefined) {
-  if (isNaN(Number(limit)) || Number(limit) !== parseInt(limit, 10)) {
-    exitWithErrorMessage(process, `--limit must be integer.`);
-  }
-}
-
-Promise.all([
-  apiUser.getRecentTracks(apiKey!, user!, Number(limit)),
-  apiUser.getTopAlbums(apiKey!, user!, period as Period, Number(limit)),
-  apiUser.getTopArtists(apiKey!, user!, period as Period, Number(limit)),
-  apiUser.getTopTracks(apiKey!, user!, period as Period, Number(limit)),
-])
-.then(([
-  recentTracks,
-  topAlbums,
-  topArtists,
-  topTracks,
-]) => {
-  process.stdout.write(
-    [
-      tableFormatter.recentTracks(recentTracks),
-      tableFormatter.topAlbums(topAlbums),
-      tableFormatter.topArtists(topArtists),
-      tableFormatter.topTracks(topTracks),
-    ].join('\n'),
-  );
-  process.exit(0);
-})
-.catch(e => {
-  exitWithErrorMessage(process, `${e.message}`);
-});
